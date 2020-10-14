c9b1423bf187a7a42ce25172907f86fcca52edd8
Zamora <jpzamora49@misena.edu.co>
2020-10-13 19:05:25 -0500

    JZ Producciones v1.5.4

--- enviar.php
+++ enviar.php
@@ -1,21 +1,26 @@
-<?php 
-
-// Llamando a los campos
+<?php
+header("Content-type: text/html;charset=\"utf-8\"");
 $nombre = $_POST['nombre'];
-$email = $_POST['email'];
-$titulo = $_POST['asunto'];
-$mensaje = $_POST['mensaje'];
+$mail = $_POST['email'];
+$asunto = $_POST['asunto'];
+$empresa = $_POST['mensaje'];
 
-// Datos para el correo
-$destinatario = "jzamora11p@gmail.com";
-$asunto = "Contacto JZ Producciones";
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
-$carta = "De: $nombre \n";
-$carta .= "Correo electrónico: $email \n";
-$carta .= "Asunto: $asunto \n";
-$carta .= "Mensaje: $mensaje";
+$mensaje = "Nombre: " . $nombre . ",\r\n";
+$mensaje .= "Correo electrónico: " . $mail . " \r\n";
+$mensaje .= "Asunto: " . $asunto . " \r\n";
+$mensaje .= "Mensaje: " . $empresa . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
 
-// Enviando mensaje
-mail($destinatario, $asunto, $carta);
+$para = 'jzamora11p@gmail.com';
+$asunto = 'Contacto JZ Producciones';
+
+if (mail($para, $asunto, utf8_decode($mensaje), $header))
+echo "<script type='text/javascript'>alert('Tu mensaje ha sido enviado exitosamente');</script>";
+echo "<script type='text/javascript'>window.location.href='https://zamora11.github.io/jzproducciones/';</script>";
 
 ?>
